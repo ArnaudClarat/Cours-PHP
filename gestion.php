1f9fd5bdc7ab21ded3ff050bee9b97a4ac6dc6b9
Arnaud Clarat <arnaud.clarat@live.fr>
2019-10-10 23:09:28 +0200

    Gestion.php en cours / update1 encours

--- gestion.php
+++ gestion.php
@@ -4,7 +4,11 @@
 
     if(isset($_POST['delete1_x'])){
         $id = $_SESSION['id1'];
-//TODO Finir ici
-        echo $id;
-        var_dump($id);
+        $sql = 'DELETE FROM t_produits WHERE id_prod = ' . $id;
+        $pdo->exec($sql);
+    }
+    elseif (isset($_POST['update1_x'])){
+        $id = $_SESSION['id1'];
+        $result = $pdo->query('SELECT * FROM t_produits WHERE id_prod = '.$id);
+        var_dump($result);
     }
