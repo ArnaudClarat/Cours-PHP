e587d589b9578c66298894d93e11039db3ca10a2
Arnaud Clarat <arnaud.clarat@live.fr>
2020-02-18 17:16:35 +0100

    Ajout

---
+++ bacinfo2_panier/views/templates_c/4def049c026c2c1252be68d942cffa52dc2f8286_0.file.search.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 3.1.33, created on 2020-02-18 13:53:03
+  from 'C:\wamp64\www\bacinfo2_panier\views\templates\search.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5e4bec3f6e1ae2_99437765',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '4def049c026c2c1252be68d942cffa52dc2f8286' => 
+    array (
+      0 => 'C:\\wamp64\\www\\bacinfo2_panier\\views\\templates\\search.tpl',
+      1 => 1582031494,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e4bec3f6e1ae2_99437765 (Smarty_Internal_Template $_smarty_tpl) {
+?>search<?php }
+}
