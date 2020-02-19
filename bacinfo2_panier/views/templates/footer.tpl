8f266e5123c8c4ac9c9fb7b47297a1f07765c214
Arnaud Clarat <arnaud.clarat@live.fr>
2020-02-19 23:16:51 +0100

    Lien contact dans le footer.tpl

--- bacinfo2_panier/views/templates/footer.tpl
+++ bacinfo2_panier/views/templates/footer.tpl
@@ -1,7 +1,11 @@
 <!-- Footer -->
+<br><br><br>
 <footer class="footer fixed-bottom bg-light">
-    <div class="footer-copyright text-center py-3 ">© 2020 Copyright:
+    <div class="footer-copyright text-center py-3">
+        © 2020 Copyright:
         <a href="#">BAC INFO 2</a>
+        |
+        <a href="contact">Contactez-nous</a>
     </div>
 </footer>
 <!-- Footer -->
--- bacinfo2_panier/views/templates_c/1bdeb5735ac69f592021ec5b8a6495f8447d1c94_0.file.footer.tpl.php
+++ bacinfo2_panier/views/templates_c/1bdeb5735ac69f592021ec5b8a6495f8447d1c94_0.file.footer.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2020-02-19 18:29:21
+/* Smarty version 3.1.33, created on 2020-02-19 22:16:04
   from 'C:\wamp64\www\bacinfo2_panier\views\templates\footer.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5e4d7e81d80f21_41075576',
+  'unifunc' => 'content_5e4db3a45103f9_62486852',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '1bdeb5735ac69f592021ec5b8a6495f8447d1c94' => 
     array (
       0 => 'C:\\wamp64\\www\\bacinfo2_panier\\views\\templates\\footer.tpl',
-      1 => 1582136959,
+      1 => 1582150561,
       2 => 'file',
     ),
   ),
@@ -20,11 +20,15 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5e4d7e81d80f21_41075576 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5e4db3a45103f9_62486852 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!-- Footer -->
+<br><br><br>
 <footer class="footer fixed-bottom bg-light">
-    <div class="footer-copyright text-center py-3 ">© 2020 Copyright:
+    <div class="footer-copyright text-center py-3">
+        © 2020 Copyright:
         <a href="#">BAC INFO 2</a>
+        |
+        <a href="contact">Contactez-nous</a>
     </div>
 </footer>
 <!-- Footer -->
